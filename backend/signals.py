3fb379afe77acfa29bf0eb85b306b59b705e7d5f
Daria <daria.19.07.1993@gmail.com>
2026-02-22 18:45:05 +0300

    refactor: replace direct email sending with Celery tasks in signals

--- backend/signals.py
+++ backend/signals.py
@@ -1,8 +1,7 @@
-from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
 from django.dispatch import receiver, Signal
 from django_rest_passwordreset.signals import reset_password_token_created
-from backend.models import ConfirmEmailToken, User
+from backend.models import User
+from backend.tasks import send_email_task
 
 # Создаем сам сигнал
 new_order_signal = Signal()
@@ -10,26 +9,24 @@ new_order_signal = Signal()
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
     """
-    Отправка письма при сбросе пароля
+    Отправка письма при сбросе пароля через Celery
     """
-    msg = EmailMultiAlternatives(
-        f"Password Reset Token for {reset_password_token.user}",
-        reset_password_token.key,
-        settings.EMAIL_HOST_USER,
-        [reset_password_token.user.email]
+    # Вызываем фоновую задачу
+    send_email_task.delay(
+        subject=f"Password Reset Token for {reset_password_token.user}",
+        message=reset_password_token.key,
+        to_email=reset_password_token.user.email
     )
-    msg.send()
 
 @receiver(new_order_signal)
 def new_order_notification(user_id, **kwargs):
     """
-    Отправка письма при создании заказа
+    Отправка письма при создании заказа через Celery
     """
     user = User.objects.get(id=user_id)
-    msg = EmailMultiAlternatives(
-        f"Обновление статуса заказа",
-        'Заказ оформлен',
-        settings.EMAIL_HOST_USER,
-        [user.email]
+    # Вызываем фоновую задачу
+    send_email_task.delay(
+        subject="Обновление статуса заказа",
+        message="Заказ оформлен",
+        to_email=user.email
     )
-    msg.send()
